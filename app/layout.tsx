7409153e410f52fa5b56076bb8e0d046f8e960cf
Sketchbrahma 1 <sketchbrahma1@Viswas-MacBook-Air.local>
2026-09-10 22:44:15 +0530

    style: replace DM Sans with Inter Tight globally

--- app/layout.tsx
+++ app/layout.tsx
@@ -69,7 +69,7 @@ export default function RootLayout({
       <head>
         <link rel="preconnect" href="https://fonts.googleapis.com" />
         <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="anonymous" />
-        <link href="https://fonts.googleapis.com/css2?family=DM+Sans:wght@400;500;600;700;800&display=swap" rel="stylesheet" />
+        <link href="https://fonts.googleapis.com/css2?family=Inter+Tight:wght@400;500;600;700;800&display=swap" rel="stylesheet" />
       </head>
       <body className="antialiased">{children}</body>
     </html>
